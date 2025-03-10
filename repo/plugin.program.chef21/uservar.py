df54d7a1299a043f39528e849ffd315c786e4604
sweetkitten27 <lukestot0103@gmail.com>
2025-03-10 16:50:45 -0700

    update all files

---
+++ repo/plugin.program.chef21/uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://lusd1.org/kodi/builds/build-addons/builds.xml'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://raw.githubusercontent.com/Buildtexts69/omegabuilds/main/notify.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
