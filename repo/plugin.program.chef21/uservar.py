67339eab451cea8d079e08ee4bb8a44359ba39e5
sweetkitten27 <lukestot0103@gmail.com>
2025-03-10 18:43:54 -0700

    updated Source Code Of One Of The Plugins

--- repo/plugin.program.chef21/uservar.py
+++ repo/plugin.program.chef21/uservar.py
@@ -1,7 +1,7 @@
 import xbmcaddon
 
 '''#####-----Build File-----#####'''
-buildfile = 'https://lusd1.org/kodi/builds/build-addons/builds.xml'
+buildfile = 'https://www.dropbox.com/scl/fi/wfizagdfaosouzjinvdg3/builds.xml?rlkey=f5hsfu62zzscd8jgxrfbivijj&st=p50g8bzx&dl=1'
 
 '''#####-----Notifications File-----#####'''
 notify_url  = 'https://raw.githubusercontent.com/Buildtexts69/omegabuilds/main/notify.txt'
